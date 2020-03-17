7ede3836bfc60c1b98f658c0c9e7ce60dffb16c8
Lukas Forst <lukas@forst.pw>
2020-03-17 21:57:15 +0100

    use json directly

--- roman/RomanClient.py
+++ roman/RomanClient.py
@@ -1,5 +1,3 @@
-import json
-
 import requests
 
 from common.Config import Config
@@ -10,8 +8,7 @@ class RomanClient:
         self.url = config.roman_url
 
     def send_message(self, token: str, payload: dict) -> str:
-        r = requests.post(f"{self.url}/conversation", data=json.dumps(payload),
-                          headers={"content-type": "application/json", "Authorization": f"Bearer {token}"})
+        r = requests.post(f"{self.url}/conversation", json=payload, headers={"Authorization": f"Bearer {token}"})
         return r.json()['messageId']
 
     def get_conversation_info(self, token: str) -> dict:
