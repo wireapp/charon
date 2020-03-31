a38e2a226a7269b6077203f5324675aeb68d7e1e
Lukas Forst <lukas@forst.pw>
2020-03-31 20:14:32 +0200

    add some documentation

--- common/Config.py
+++ common/Config.py
@@ -11,11 +11,20 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class Config:
+    """
+    Where is the Roman running.
+    """
     roman_url: str
 
+    """
+    Redis configuration
+    """
     redis_url: str
     redis_port: int
 
+    """
+    URL of the Charon - to generate webhook links correctly
+    """
     charon_url: str
 
 
@@ -30,7 +39,8 @@ def build_configuration() -> Config:
     """
     Builds configuration from environment or from the Flask properties
     """
-    return Config(roman_url=get_prop('ROMAN_URL'), redis_url=get_prop('REDIS_URL'),
+    return Config(roman_url=get_prop('ROMAN_URL'),
+                  redis_url=get_prop('REDIS_URL'),
                   redis_port=int(get_prop('REDIS_PORT')),
                   charon_url=get_prop('CHARON_URL'))
 
@@ -39,8 +49,8 @@ def get_prop(name: str) -> str:
     """
     Gets property from environment or from the flask env.
     """
-    env = os.environ.get(name, app.config.get(name))
-    if not env:
-        logger.error('It was not possible to retrieve configuration!')
-        raise EnvironmentError('No existing configuration found!')
-    return env
+    config = os.environ.get(name, app.config.get(name))
+    if not config:
+        logger.error(f'It was not possible to retrieve configuration for property "{name}"!')
+        raise EnvironmentError(f'No existing configuration for "{name}" found!')
+    return config
